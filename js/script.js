77c0ec4e45f131af5ce3b16bcbaaa34b375a3006
RUSICH <artem.rusich@yandex.r>
2016-02-18 12:04:43 +0300

    update map

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ var map__modal = document.querySelector(".map__modal");
 var popup__overlay = document.querySelector(".popup__overlay");
 var map__modal_close = document.querySelector(".map__modal-close");
 
+if(body.classList.contains('nojs')){
+	body.classList.remove("nojs");
+	body.classList.add("js");
+}
+
 btn_slide_1.addEventListener("click", function(event) {
 	event.preventDefault();
 	btn_slide_2.classList.remove("active");
@@ -65,54 +70,3 @@ map__modal_close.addEventListener("click", function(event) {
 	popup__overlay.classList.toggle("active");
 });
 
-/*
-var link = document.querySelector(".login");
-
-var popup = document.querySelector(".modal-content");
-var close = popup.querySelector(".modal-content-close");
-
-var form = popup.querySelector("form");
-var login = popup.querySelector("[name=login]");
-var password = popup.querySelector("[name=password]");
-
-var storage = localStorage.getItem("login");
-
-link.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.add("modal-content-show");
-
-if (storage) {
-  login.value = storage;
-  password.focus();
-} else {
-  login.focus();
-}
-
-});
-
-close.addEventListener("click", function(event) {
-event.preventDefault();
-popup.classList.remove("modal-content-show");
-popup.classList.remove("modal-error");
-});
-
-form.addEventListener("submit", function(event) {
-if (!login.value || !password.value) {
-  event.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
-} else {
-  localStorage.setItem("login", login.value);
-}
-});
-
-window.addEventListener("keydown", function(event) {
-if (event.keyCode === 27) {
-	if (popup.classList.contains("modal-content-show")) {
-		popup.classList.remove("modal-content-show");
-		popup.classList.remove("modal-error");
-	}
-}
-});
-*/
